5a63caf8793a8d0eb4889ac3bc01492863093688
nemrood <41047129+nemrood@users.noreply.github.com>
2019-06-24 02:01:46 +0100

    Update init.sh

--- iac/terraform/init.sh
+++ iac/terraform/init.sh
@@ -7,4 +7,4 @@ export ARM_SUBSCRIPTION_ID=$3
 export ARM_TENANT_ID=$4
 export ARM_ACCESS_KEY=$5
 echo "Run"
-terraform init  -backend-config=backend.tfvars
+terraform init -backend-config=backend.tfvars
